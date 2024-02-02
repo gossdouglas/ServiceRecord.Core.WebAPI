a93c4432c30564c3a6f390af13765d8b3f33d705
gossdouglas <gossdouglas@yahoo.com>
2024-02-01 18:55:17 -0600

    Update ApplicationDbContext.cs

--- Visual Studio/ServiceRecord.Core.WebAPI/DatabaseContext/ApplicationDbContext.cs
+++ Visual Studio/ServiceRecord.Core.WebAPI/DatabaseContext/ApplicationDbContext.cs
@@ -37,14 +37,6 @@ namespace ServiceRecord.Core.WebAPI.DatabaseContext
             modelBuilder.Entity<JobSubJob>()
             .HasKey(x => new { x.JobID, x.SubJobID });
 
-            //modelBuilder.Entity<City>().HasData(
-            // new City() { CityID = Guid.Parse("BF160CFD-E693-4C6A-9417-037B4501EC9B"), CityName = "New York" });
-
-            //modelBuilder.Entity<City>().HasData(
-            // new City() { CityID = Guid.Parse("858462EF-5587-48D5-8DB3-392938699F42"), CityName = "London" });
-
-            //import some customers
-            //import
         }
     }
 }
