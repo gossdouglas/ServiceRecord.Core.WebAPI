c31cbb3d14f23de19080969fa009bb895b86b5c1
gossdouglas <gossdouglas@yahoo.com>
2024-02-22 13:48:27 -0600

    going to shift the projects around and create a new repository that holds react and the back end.

--- Visual Studio/ServiceRecord.Core.WebAPI/React/React front-end with a .NET Web API back-end application/Front-end/src/components/Home.tsx
+++
@@ -1,6 +0,0 @@
-
-const Home = () => {
-    return <h1 className="text-center">Home</h1>
-};
-
-export default Home;
--- Visual Studio/ServiceRecord.Core.WebAPI/React/React front-end with a .NET Web API back-end application/Front-end/src/misc/fixed.ts
+++
@@ -1,3 +0,0 @@
-const url = 'https://localhost:7201/api/Employee' as const;
-
-export default url
